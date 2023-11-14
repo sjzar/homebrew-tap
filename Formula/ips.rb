2eb27ed661a9fc04a0fd1922078517de4b4ac28d
Shen Junzheng <shenjunzheng@gmail.com>
2023-11-14 11:36:56 +0800

    ips v0.3.0

--- Formula/ips.rb
+++ Formula/ips.rb
@@ -3,8 +3,8 @@
 class Ips < Formula
     desc "ips is a command-line tool and library that facilitates the querying, dumping, and packaging of IP geolocation databases."
     homepage "https://github.com/sjzar/ips"
-    url "https://github.com/sjzar/ips/archive/refs/tags/v0.2.6.tar.gz"
-    sha256 "508222ff5c62a523c9f8393108614d072869aecf54569883356668a4d9ef0cf2"
+    url "https://github.com/sjzar/ips/archive/refs/tags/v0.3.0.tar.gz"
+    sha256 "f0983d11bdfe23c7effcc4d185d6bd97e8badc422d11279e8831fbe92b2529a3"
     head "https://github.com/sjzar/ips.git"
   
     depends_on "go" => :build
